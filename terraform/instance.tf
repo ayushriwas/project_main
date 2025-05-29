0dc7046fd4714ab32d360fc798bbd51e03c22abb
ayushriwas <ayushriwas@gmail.com>
2025-05-29 13:12:44 +0530

    added s3 using terraform

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -58,6 +58,18 @@ resource "aws_security_group" "ocr_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    security_groups  = []
+    self             = false
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
