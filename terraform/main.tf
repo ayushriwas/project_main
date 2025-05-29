d3cb557fb75d11fab9827ef0c8eedefbb45d60ca
ayushriwas <ayushriwas@gmail.com>
2025-05-29 13:00:17 +0530

    added s3 using terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,28 +2,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_instance" "ocr_server" {
-  ami                    = var.ami_id
-  instance_type          = var.instance_type
-  key_name               = var.key_name
-  associate_public_ip_address = true
-
-  vpc_security_group_ids = [aws_security_group.ocr_sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo apt-get update -y
-              sudo apt-get install -y docker.io
-              sudo systemctl start docker
-	      sudo docker pull ${var.docker_image}
-              sudo docker run -d -p 5000:5000 ${var.docker_image}
-              EOF
-
-  tags = {
-    Name = "OCR-Server"
-  }
-}
-
 resource "aws_security_group" "ocr_sg" {
   name        = "ocr_sg"
   description = "Allow SSH and web traffic"
@@ -34,30 +12,6 @@ resource "aws_security_group" "ocr_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = []
-    prefix_list_ids  = []
-    security_groups  = []
-    self             = false
-  }
-
-  ingress {
-    description = "Web"
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = []
-    prefix_list_ids  = []
-    security_groups  = []
-    self             = false
-  }
-
-  egress {
-    description = "Allow all outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
     ipv6_cidr_blocks = []
     prefix_list_ids  = []
