57a66441fe87fe402c6b14648d357a25c453dfcd
ayushriwas <ayushriwas@gmail.com>
2025-05-30 21:30:18 +0530

    added lambda function

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,55 +1,47 @@
-# IAM Role for Lambda
-resource "aws_iam_role" "ocr_lambda_exec" {
-  name = "ocr-lambda-exec-role"
-
+# IAM role for EC2
+resource "aws_iam_role" "ocr_ec2_role" {
+  name = "ocr-ec2-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
   })
 }
 
-# IAM Policy for Lambda S3 Access and CloudWatch Logging
-resource "aws_iam_policy" "ocr_lambda_policy" {
-  name        = "ocr-lambda-access-policy"
-  description = "Allows Lambda to read from S3 and write to CloudWatch"
-
+# IAM policy for EC2 to access S3
+resource "aws_iam_policy" "ocr_s3_policy" {
+  name        = "ocr-s3-access-policy"
+  description = "Allows EC2 to access S3 buckets"
   policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "s3:GetObject",
-          "s3:ListBucket"
-        ],
-        Resource = [
-          "arn:aws:s3:::ocr-images-bucket-*",
-          "arn:aws:s3:::ocr-images-bucket-*/*"
-        ]
-      },
-      {
-        Effect = "Allow",
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        Resource = "*"
-      }
-    ]
+    Statement = [{
+      Effect = "Allow",
+      Action = [
+        "s3:PutObject",
+        "s3:GetObject",
+        "s3:ListBucket"
+      ],
+      Resource = [
+        "arn:aws:s3:::ocr-images-bucket-*",
+        "arn:aws:s3:::ocr-images-bucket-*/*"
+      ]
+    }]
   })
 }
 
-# Attach Policy to Lambda Role
-resource "aws_iam_role_policy_attachment" "attach_lambda_policy" {
-  role       = aws_iam_role.ocr_lambda_exec.name
-  policy_arn = aws_iam_policy.ocr_lambda_policy.arn
+# Attach EC2 policy to role
+resource "aws_iam_role_policy_attachment" "attach_policy" {
+  role       = aws_iam_role.ocr_ec2_role.name
+  policy_arn = aws_iam_policy.ocr_s3_policy.arn
+}
+
+# ✅ THIS IS CRUCIAL (used in `instance.tf`)
+resource "aws_iam_instance_profile" "ocr_instance_profile" {
+  name = "ocr-instance-profile"
+  role = aws_iam_role.ocr_ec2_role.name
 }
