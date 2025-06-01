291e6a8e9f6d4dbe8f52b12f4506262c2f069db0
ayushriwas <ayushriwas@gmail.com>
2025-06-01 17:31:21 +0530

    added lambda

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -11,28 +11,32 @@ resource "aws_instance" "ocr_server" {
   iam_instance_profile        = aws_iam_instance_profile.ocr_instance_profile.name
   vpc_security_group_ids      = [aws_security_group.ocr_sg.id]
 
-  user_data = <<-EOF
+user_data = <<-EOF
               #!/bin/bash
+              exec > /var/log/user-data.log 2>&1
+              set -x
 
-              # Update and install Docker
-              sudo apt update 
-              sudo apt install -y docker.io
+              # Update package list and install Docker
+              apt-get update -y
+              apt-get install -y docker.io
 
               # Enable and start Docker
-              sudo systemctl enable docker
-              sudo systemctl start docker
+              systemctl enable docker
+              systemctl start docker
 
-              # Add default user to Docker group (Ubuntu AMI)
-              sudo usermod -aG docker ubuntu
-	      sudo usermod -aG docker admin	
-              sudo systemctl restart docker 
-	      # Wait a few seconds to ensure Docker is ready
+              # Add the 'admin' user to Docker group
+              usermod -aG docker admin
+
+              # Restart Docker to ensure group change takes effect
+              systemctl restart docker
+
+              # Wait for Docker to become ready
               sleep 15
 
-              # Pull and run Docker container
+              # Pull and run your Docker container
               docker pull ayush5626/ocr_web
               docker run --name ocr -d -p 5000:5000 ayush5626/ocr_web
-              EOF
+EOF
 
   tags = {
     Name = "OCR-Server"
