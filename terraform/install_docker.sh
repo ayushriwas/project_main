b0d3361ebc30794b0bd33366b66e7ffe6530586b
ayushriwas <ayushriwas@gmail.com>
2025-06-03 00:46:33 +0530

    added lambda

--- terraform/install_docker.sh
+++ terraform/install_docker.sh
@@ -16,8 +16,9 @@ usermod -aG docker admin || true
 # Pull and run your container (IAM role provides AWS credentials)
 docker pull ayush5626/ocr_web
 docker run -d --name ocr \
+  --network=host 
   -e S3_BUCKET=ocr-images-bucket-e6a2ac1e \
   -e S3_REGION=us-east-1 \
   -p 5000:5000 \
   ayush5626/ocr_web
-docker start ocr 
+
